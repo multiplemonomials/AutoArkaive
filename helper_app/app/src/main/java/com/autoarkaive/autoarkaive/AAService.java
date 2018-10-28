d2b10a09dc6fedb7b999584d753b618a7c4c3e8a
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2018-10-28 00:14:32 -0700

    Get initial test of inspecting UI via accessibility service going

--- helper_app/app/src/main/java/com/autoarkaive/autoarkaive/AAService.java
+++ helper_app/app/src/main/java/com/autoarkaive/autoarkaive/AAService.java
@@ -1,6 +1,7 @@
 package com.autoarkaive.autoarkaive;
 
 import android.accessibilityservice.AccessibilityService;
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -15,30 +16,54 @@ public class AAService extends AccessibilityService
 {
 	private final String TAG = "AAService";
 
+	public static final int FOREGROUND_SERVICE_ID = 101;
+
     public AAService()
     {
     }
 
 	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		Notification notification = new Notification.Builder(this)
+				.setContentTitle("AutoArkaive")
+				.setContentText("Service Running")
+				.setSmallIcon(R.drawable.ic_launcher_foreground)
+				.build();
+		startForeground(FOREGROUND_SERVICE_ID, notification);
+
+		return START_STICKY;
+	}
+
+	@Override
 	protected void onServiceConnected()
 	{
 		super.onServiceConnected();
+		Log.d(TAG, "onServiceConnected() called");
+	}
+
+	@Override
+	public void onDestroy()
+	{
+		super.onDestroy();
+		Log.w(TAG, "Destroyed!");
+		stopForeground(true);
 	}
 
 	@Override
     public void onAccessibilityEvent(AccessibilityEvent event)
     {
-		Log.d(TAG, "Caught accessibility event: " + event.toString());
+		Log.i(TAG, "Caught accessibility event: " + event.toString());
 
 		switch (event.getEventType())
 		{
 			//On Gesture events print out the entire view heirarchy!
 			case AccessibilityEvent.TYPE_GESTURE_DETECTION_START:
 				Log.d(TAG, A11yNodeInfo.wrap(getRootInActiveWindow()).toViewHeirarchy());
-
+				break;
 			case AccessibilityEvent.TYPE_VIEW_CLICKED:
 				Log.d(TAG, event.getSource().toString());
-
+				break;
 			default: {
 				//The event has different types, for you, you want to look for "action clicked"
 				if (event.getSource() != null) {
--- helper_app/app/src/main/java/com/autoarkaive/autoarkaive/MainActivity.java
+++ helper_app/app/src/main/java/com/autoarkaive/autoarkaive/MainActivity.java
@@ -3,19 +3,33 @@ package com.autoarkaive.autoarkaive;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+	private static final Intent sSettingsIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
 
 	final static String TAG = "AAMainActivity";
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // start the accessibility service
-        startService(new Intent(this, AAService.class));
-        Log.d(TAG, "Service Started!");
+
+		// start the accessibility service
+		startService(new Intent(this, AAService.class));
+		Toast.makeText(this, "Service started!", Toast.LENGTH_SHORT).show();
+
+		startActivity(sSettingsIntent);
     }
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+
+	}
 }
