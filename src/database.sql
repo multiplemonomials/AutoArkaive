a90b84e26f017574cbde03efdcc8acf5e6d67e2b
Anna Szymanski <annaszymanski@Annas-MacBook-Pro-5.local>
2018-10-28 20:23:07 -0700

    database changes

--- src/database.sql
+++ src/database.sql
@@ -6,15 +6,18 @@ CREATE TABLE myUsers (
 	fullname VARCHAR(60) PRIMARY KEY NOT NULL,
 	email VARCHAR(60) NOT NULL,
 	picurl VARCHAR(300)
+	arkaive_username VARCHAR(515) NOT NULL,
+	arkaive_password VARCHAR(515) NOT NULL
 );
 
 
 CREATE TABLE myClasses(
-	classtime VARCHAR() NOT NULL,
+	checkinStartTime VARCHAR(300) NOT NULL,
+	checkinEndTime VARCHAR(300) NOT NULL,
 	latitude FLOAT(20,8) NOT NULL, --we can update this based on how precise the api gives us
 	longitude FLOAT(20,8) NOT NULL, -- https://stackoverflow.com/questions/7167604/how-accurately-should-i-store-latitude-and-longitude
 	altitude FLOAT(20,8) NOT NULL,
-	classcode VARCHAR(6) NOT NULL,
+	courseCode VARCHAR(6) NOT NULL,
 	classname VARCHAR(40) NOT NULL
 );
 
@@ -28,6 +31,7 @@ int altitude - altitude in meters
 String username, password - Arkaive login info
 String courseCode - Course code (
 LocalTime checkinStartTime, checkinEndTime - Time window where checkin can occur.
+
 Note: must use Joda Time’s LocalTime instead of Java’s because Android does not support Java 8 yet 
 
 */
